cd7a69230f103acbc9e01643a55e70e1fbc88170
zuaam <46837548+zuaam@users.noreply.github.com>
2019-10-09 03:35:49 -0700

    Updates

---
+++ precache-manifest.78e1b73be95dfdd1a144703c4742f5a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272e1d3b1dc9e3023f32699bb1a6daf3",
+    "url": "/static-react-web-application/index.html"
+  },
+  {
+    "revision": "b8d61ff7a55912755f82",
+    "url": "/static-react-web-application/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "97119b114fb04fd8a681",
+    "url": "/static-react-web-application/static/js/2.0c89e1ea.chunk.js"
+  },
+  {
+    "revision": "b8d61ff7a55912755f82",
+    "url": "/static-react-web-application/static/js/main.6789dc00.chunk.js"
+  },
+  {
+    "revision": "f12c9164ed15cadf1d18",
+    "url": "/static-react-web-application/static/js/runtime-main.1003d8cf.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/static-react-web-application/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/static-react-web-application/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/static-react-web-application/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
