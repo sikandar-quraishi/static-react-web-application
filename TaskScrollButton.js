ea7303ab7e54fd8f3b48b8a9cf04b2c47f64c942
Sikandar Quraishi <46837548+sikandar-quraishi@users.noreply.github.com>
2020-02-23 03:09:10 -0800

    Add files via upload

---
+++ TaskScrollButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import NavigationIcon from "@material-ui/icons/Navigation";
+import { scroller } from "react-scroll";
+
+const TaskScrollButton = () => {
+  const scrollToElement = element => {
+    scroller.scrollTo(element, {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: -134
+    });
+  };
+  return (
+    <div>
+      <Button
+        onClick={() => scrollToElement("Featured")}
+        size="small"
+        color="secondary"
+        aria-label="add"
+        style={{ backgroundColor: "#000000", color: "white" }}
+      >
+        <NavigationIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default TaskScrollButton;
